9ad84b8bdb542da0b88c408398164a91847de054
vishvishu <vishvishu14@gmail.com>
2017-04-09 02:40:15 +0530

    Dynamic Programming- 1

---
+++ Longest_Increasing_Subsequence.py
@@ -0,0 +1,29 @@
+def search(arr,l,r,key):
+    while(r-l>1):
+        m = l+(r-l)//2
+        if (arr[m] >= key):
+            r = m
+        else:
+            l = m
+    return r
+
+def sequence(arr):
+    if len(arr)==0:
+        return 0
+    tail = [0]*len(arr)
+    length = 1
+    tail[0] = arr[0]
+
+    for i in range(1,len(arr)):
+        if arr[i] < tail[0]:
+            tail[0] = arr[i]
+        elif arr[i] > tail[length - 1]:
+            tail[length]=arr[i]
+            length +=1
+        else:
+            tail[search(tail, -1, length-1, arr[i])] = arr[i]
+
+    return length
+
+arr = [2,5,3,7,11,8,10,13,6]
+print(sequence(arr))
