9933a817a55dd668761d8ccf20da0dc17c099b56
vishvishu <vishvishu14@gmail.com>
2017-04-29 16:48:58 +0530

    Dynamic Programming 8

--- Cutting_Rod_to_Maximize_Profit.py
+++ Cutting_Rod_to_Maximize_Profit.py
@@ -1,22 +1,16 @@
-def profit(total, arr, values):
-    n = len(arr)
-    lis = [[0]*(total+1) for i in range(n)]
+def profit(price, n):
+    val = [0 for x in range(n+1)]
+    val[0] = 0
 
-    for i in range(1, total+1):
-        if arr[0] >= i and arr[0]%i==0:
-            lis[0][i] = (i//arr[0])*values[0]
-
-    for i in range(2,n+1):
-        for j in range(1,total+1):
-            if arr[i-1] >= j:
-                lis[i][j-1] = max(lis[i-1][j-1] , lis[i][j-arr[i-1]-1] + values[i-1])
-            else:
-                lis[i][j-1] = lis[i-1][j-1]
-
-    return lis[n-1][total]
+    for i in range(1, n+1):
+        max_val = INT_MIN
+        for j in range(i):
+             max_val = max(max_val, price[j] + val[i-j-1])
+        val[i] = max_val
+ 
+    return val[n]
 
 arr = list(map(int,input().split()))
-values = list(map(int,input().split()))
 total = int(input())
 
-print(profit(total, arr, values))
+print(profit(arr, total))
